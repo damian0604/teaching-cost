af0108a54e055d6222b80ef33f263252700a7054
damian0604 <d.c.trilling@uva.nl>
2024-06-12 10:56:42 +0200

    cleanup, remove irrelevant stuff

--- render.sh
+++ render.sh
@@ -5,6 +5,8 @@ echo "This script depends on  https://github.com/jbgruber/quarto-targetlang; see
 #quarto render chapter08.qmd --no-execute --to targetlang-ipynb -o chapter08-r.ipynb --metadata target_lang:r
 #quarto render chapter08.qmd --to html
 
-quarto render chapter11.qmd --no-execute --to targetlang-ipynb -o chapter11-py.ipynb --metadata target_lang:python
-quarto render chapter11.qmd --no-execute --to targetlang-ipynb -o chapter11-r.ipynb --metadata target_lang:r
-quarto render chapter11.qmd --to html
+# remove lines inidcating special classes
+sed '/^:::/d' chapter11.qmd > chapter11-fix.qmd
+quarto render chapter11-fix.qmd --no-execute --to targetlang-ipynb -o chapter11-py.ipynb --metadata target_lang:python
+quarto render chapter11-fix.qmd --no-execute --to targetlang-ipynb -o chapter11-r.ipynb --metadata target_lang:r
+#quarto render chapter11.qmd --to html
